798c00a7535077673cdae3455ebb56af5a45f16e
Isaac Emilien <isaac.emilien@gmail.com>
2025-08-02 23:49:37 +0100

    Fixed formating issue

--- build-setpos-binds.py
+++ build-setpos-binds.py
@@ -9,7 +9,7 @@ dyn_output = ''
 
 for i in range(len(user_input) - 1):
     if i == 0: continue 
-    dyn_output += f'alias "pos{i + 1}" "{user_input[i]}; alias prevPos pos{i}; alias nextPos pos{i + 2};"'
+    dyn_output += f'alias "pos{i + 1}" "{user_input[i]}; alias prevPos pos{i}; alias nextPos pos{i + 2};"\n'
 
 output = f'''
 // Aliases 
@@ -17,8 +17,7 @@ alias "prevPos" "pos{len(user_input)}"
 alias "nextPos" "pos2"
 
 alias "pos1" "{user_input[0]}; alias prevPos pos{len(user_input)}; alias nextPos pos2;"
-{dyn_output}
-alias "pos{len(user_input)}" "{user_input[len(user_input) - 1]}; alias prevPos pos{len(user_input) - 1}; alias nextPos pos1;"
+{dyn_output}alias "pos{len(user_input)}" "{user_input[len(user_input) - 1]}; alias prevPos pos{len(user_input) - 1}; alias nextPos pos1;"
 
 // Binds 
 bind "mouse4" "prevPos" 
