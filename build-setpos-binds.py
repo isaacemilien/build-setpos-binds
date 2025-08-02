8c26e9184c1ce9dd0423120982033bc3e0a1a9f5
Isaac Emilien <isaac.emilien@gmail.com>
2025-08-02 23:32:40 +0100

    Converted to dyn output

--- build-setpos-binds.py
+++ build-setpos-binds.py
@@ -5,18 +5,19 @@ if len(sys.argv) < 2:
 
 user_input = sys.argv[1:]
 
+dyn_output = ''
+
+for i in range(len(user_input) - 1):
+    if i == 0: continue 
+    dyn_output += f'alias "pos{i + 1}" "{user_input[i]}; alias prevPos pos{i}; alias nextPos pos{i + 2};"'
+
 output = f'''
 // Aliases 
 alias "prevPos" "pos{len(user_input)}" 
 alias "nextPos" "pos2"
 
-alias "pos1" "{user_input[0]}; alias prevPos pos{len(user_input)}; alias nextPos pos2;"'''
-
-for i in range(len(user_input) - 1):
-    if i == 0: continue 
-    output += f'\nalias "pos{i + 1}" "{user_input[i]}; alias prevPos pos{i}; alias nextPos pos{i + 2};"'
-
-output += f'''
+alias "pos1" "{user_input[0]}; alias prevPos pos{len(user_input)}; alias nextPos pos2;"
+{dyn_output}
 alias "pos{len(user_input)}" "{user_input[len(user_input) - 1]}; alias prevPos pos{len(user_input) - 1}; alias nextPos pos1;"
 
 // Binds 
